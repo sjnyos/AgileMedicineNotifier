6b7d49db3c2870de34b28e17e06bc89735f468fa
jarus <wrksuraj@gmail.com>
2020-06-07 13:15:22 +0545

    TEsting

---
+++ app/src/test/java/com/strawhatspirates/medicinetime/loginactivityTest/mainLoginctivityTest.java
@@ -0,0 +1,20 @@
+//package com.strawhatspirates.medicinetime.loginactivityTest;
+//
+//import org.assertj.core.api.Assertions.assertThat
+//import org.junit.Test
+//import org.junit.jupiter.api.TestInstance
+//public class mainLoginctivityTest {
+//    private val model = MainModel()
+//
+////    @Test
+////    fun fivePercentTipForOneHundredDollarsShouldReturnOneHundredAndFiveDollars() {
+////        model.setTipOption(0)
+////
+////        val price = model.computeTotalPrice("100")
+////
+////        assertThat(price).isEqualTo(105.0)
+////
+////        // JUnit assertion if you prefer
+////        // assertEquals(105.0, price)
+////    }
+//}
