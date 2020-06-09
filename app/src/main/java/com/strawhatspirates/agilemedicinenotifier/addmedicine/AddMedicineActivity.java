e0f524b623a94ad984655a634b076a841395b981
jarus <wrksuraj@gmail.com>
2020-06-09 17:52:06 +0545

    Added Medicine Activity

---
+++ app/src/main/java/com/strawhatspirates/agilemedicinenotifier/addmedicine/AddMedicineActivity.java
@@ -0,0 +1,16 @@
+package com.strawhatspirates.agilemedicinenotifier.addmedicine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.strawhatspirates.agilemedicinenotifier.R;
+
+public class AddMedicineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_medicine);
+    }
+}
