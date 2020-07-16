0af5217b597cb81d952d292977f3500e92e0b563
jarus <wrksuraj@gmail.com>
2020-07-16 08:42:45 +0545

    date wise report for history

---
+++ app/src/main/java/com/strawhatspirates/agilemedicinenotifier/report/FilterType.java
@@ -0,0 +1,11 @@
+package com.strawhatspirates.agilemedicinenotifier.report;
+
+
+public enum  FilterType {
+
+    ALL_MEDICINES,
+
+    TAKEN_MEDICINES,
+
+    IGNORED_MEDICINES
+}
---
+++ app/src/main/java/com/strawhatspirates/agilemedicinenotifier/report/MonthlyReportContract.java
@@ -0,0 +1,46 @@
+package com.strawhatspirates.agilemedicinenotifier.report;
+
+import com.strawhatspirates.medicinetime.BasePresenter;
+import com.strawhatspirates.medicinetime.BaseView;
+import com.strawhatspirates.medicinetime.data.source.History;
+
+import java.util.List;
+
+
+public interface MonthlyReportContract {
+
+    interface View extends BaseView<Presenter> {
+
+        void setLoadingIndicator(boolean active);
+
+        void showHistoryList(List<History> historyList);
+
+        void showLoadingError();
+
+        void showNoHistory();
+
+        void showTakenFilterLabel();
+
+        void showIgnoredFilterLabel();
+
+        void showAllFilterLabel();
+
+        void showNoTakenHistory();
+
+        void showNoIgnoredHistory();
+
+        boolean isActive();
+
+        void showFilteringPopUpMenu();
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+        void loadHistory(boolean showLoading);
+
+        void setFiltering(FilterType filterType);
+
+        FilterType getFilterType();
+    }
+}
